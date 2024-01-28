4f3e624b7a0530ef392ebe87b344020cdaa8207e
noto-21 <128177967+noto-21@users.noreply.github.com>
2024-01-28 18:36:42 -0500

    Progress

---
+++ PD_1/Noah/BinaryCalculator/src/main/java/com/ontariotechu/sofe3980U/App.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.sofe3980U;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
